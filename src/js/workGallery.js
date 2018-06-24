eb64fd9b5090989cd469675c02e9018437bc2d28
alexan <eder.alexan@gmail.com>
2018-06-24 10:56:23 +0200

    audit & optimization

--- src/js/workGallery.js
+++ src/js/workGallery.js
@@ -1,58 +1,58 @@
-(function() {
-    var GALLERY_SELECTOR = '.work-gallery';
-    var WORK_SELECOTR = '.work';
-    var WORK_IMAGE_SELECTOR = '.work-image';
-    var WORK_IMAGE_COPY_CLASS = 'work-image--absolute';
-    var WORK_IMAGE_COPY_SELECTOR = '.' + WORK_IMAGE_COPY_CLASS;
+(function () {
+   var GALLERY_SELECTOR = '.work-gallery';
+   var WORK_SELECOTR = '.work';
+   var WORK_IMAGE_SELECTOR = '.work-image';
+   var WORK_IMAGE_COPY_CLASS = 'work-image--absolute';
+   var WORK_IMAGE_COPY_SELECTOR = '.' + WORK_IMAGE_COPY_CLASS;
 
-    var galleries = []; 
-    
-    var workGalleries = document.querySelectorAll(GALLERY_SELECTOR);
+   var galleries = [];
 
-    workGalleries.forEach(function(workGallery) {
-        var works = workGallery.querySelectorAll(WORK_SELECOTR);
-    
-        works.forEach(function(work) {
-            var workImage = work.querySelector(WORK_IMAGE_SELECTOR);
-            var copyImage = workImage.cloneNode();
-            workImage.style.visibility = 'hidden';
-            copyImage.classList.add(WORK_IMAGE_COPY_CLASS);
+   var workGalleries = document.querySelectorAll(GALLERY_SELECTOR);
 
-            work.append(copyImage);
-        });
-        
-        var masnory = new Masonry(workGallery, {
-                itemSelector: WORK_SELECOTR,
-                resize: false,
-                originLeft: true
-        });
-        
-        imagesLoaded(workGallery).on('progress', function() {
-            layout(masnory, works);
-        });
+   workGalleries.forEach(function (workGallery) {
+      var works = workGallery.querySelectorAll(WORK_SELECOTR);
 
-        galleries.push({
-            works: works,
-            masnory: masnory
-        });
-    });
+      works.forEach(function (work) {
+         var workImage = work.querySelector(WORK_IMAGE_SELECTOR);
+         var copyImage = workImage.cloneNode();
+         workImage.style.visibility = 'hidden';
+         copyImage.classList.add(WORK_IMAGE_COPY_CLASS);
 
-    window.onresize = function(event) {
-        galleries.forEach(function(gallery) {
-            layout(gallery.masnory, gallery.works);
-        });
-    };
+         work.append(copyImage);
+      });
 
-    function layout(masnory, works) {
-        works.forEach(function(work) {
-            var workImage = work.querySelector(WORK_IMAGE_SELECTOR);
-            var copyImage = work.querySelector(WORK_IMAGE_COPY_SELECTOR);
+      var masnory = new Masonry(workGallery, {
+         itemSelector: WORK_SELECOTR,
+         resize: false,
+         originLeft: true
+      });
 
-            copyImage.setAttribute('style', 'width: ' + workImage.offsetWidth + 'px;');
-        });
-        
-        masnory.layout();
-    }
+      imagesLoaded(workGallery).on('progress', function () {
+         layout(masnory, works);
+      });
+
+      galleries.push({
+         works: works,
+         masnory: masnory
+      });
+   });
+
+   window.onresize = function (event) {
+      galleries.forEach(function (gallery) {
+         layout(gallery.masnory, gallery.works);
+      });
+   };
+
+   function layout(masnory, works) {
+      works.forEach(function (work) {
+         var workImage = work.querySelector(WORK_IMAGE_SELECTOR);
+         var copyImage = work.querySelector(WORK_IMAGE_COPY_SELECTOR);
+
+         copyImage.setAttribute('style', 'width: ' + workImage.offsetWidth + 'px;');
+      });
+
+      masnory.layout();
+   }
 })();
 
 
